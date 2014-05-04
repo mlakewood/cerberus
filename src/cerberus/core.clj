e134e305e733161154181eea72670dbdfd11f5f6
Mark Lakewood <underplank@gmail.com>
2014-05-03 18:43:31 -0700

    fixed it up apparently.

--- src/cerberus/core.clj
+++ src/cerberus/core.clj
@@ -7,11 +7,11 @@
               [com.duelinmarkers.ring-request-logging :refer [wrap-request-logging]]
               ))
 
-; (def backends (config :backends))
-(def backends [{:remote-addr "127.0.0.1" :server-port 5000}
-               {:remote-addr "127.0.0.1" :server-port 5000}
-               {:remote-addr "127.0.0.1" :server-port 5000}
-               {:remote-addr "127.0.0.1" :server-port 5000}])
+(def backends (config :backends))
+; (def backends [{:remote-addr "127.0.0.1" :server-port 5000}
+;                {:remote-addr "127.0.0.1" :server-port 5000}
+;                {:remote-addr "127.0.0.1" :server-port 5000}
+;                {:remote-addr "127.0.0.1" :server-port 5000}])
 
 (def backend-counter (atom (into [] (replicate (count backends) 0))))
 
@@ -20,11 +20,11 @@
 (defn pick-backend []
     (let [end (first (apply min-key second (map-indexed vector @backend-counter)))]
         (swap! backend-counter update-in [end] inc)
-        end))
+        (int end)))
 
 
 (defn handler [response-channel request]
-  (let [new-request (merge request (get backends pick-backend))]
+  (let [new-request (merge request (get backends 0))]
       (swap! request-save :request new-request)
       (enqueue response-channel
         (http-request new-request))))

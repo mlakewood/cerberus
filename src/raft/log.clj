3b52b7a96a667f3fa4c7aeefdbb39053d1307ab3
Mark Lakewood <underplank@gmail.com>
2014-12-20 15:47:44 -0800

    second commit

--- src/raft/log.clj
+++ src/raft/log.clj
@@ -3,6 +3,9 @@
 
 (defrecord LogEntry [index term payload])
 
+(defrecord DummyLogEntry [index term payload])
+(defrecord DummyLogEntry2 [foo bar baz])
+
 (defn append-log [log entry prev]
     (if (every? identity (map = prev (last log)))
         (conj log entry)
